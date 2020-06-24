8b6bb96204523a2c54eaf8b3244fecf2269876a6
Ranjitha S <ranjitha.s@dxc.com>
2020-06-24 10:44:23 +0530

    first commit

---
+++ src/test/java/com/dxc/inventoryapi/SpringBootDataRestApiDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.dxc.inventoryapi;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDataRestApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
